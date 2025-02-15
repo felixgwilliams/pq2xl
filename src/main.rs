cec6c20fa918d200e9c46e000be7e0b7e1ec6703
Felix Williams <felixgwilliams@users.noreply.github.com>
2025-02-15 00:40:01 +0000

    wip cli

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,25 @@
 #![allow(clippy::cargo)]
 // #![warn(missing_docs)]
 
-use std::fs::File;
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
+};
 
 use anyhow::{bail, Error};
+use clap::{
+    builder::{styling::AnsiColor, Styles},
+    command, ArgAction, Parser, ValueEnum,
+};
 use polars::prelude::*;
+
+const STYLES: Styles = Styles::styled()
+    .header(AnsiColor::Yellow.on_default())
+    .usage(AnsiColor::Green.on_default())
+    .literal(AnsiColor::Green.on_default())
+    .placeholder(AnsiColor::Green.on_default());
+
+#[derive(Debug, Clone)]
 enum Conversion {
     Pass,
     Convert(DataType),
@@ -69,36 +84,86 @@ fn process_list(c: Expr) -> Expr {
         .keep()
 }
 
+#[derive(Debug, Clone, Copy, Default, ValueEnum)]
+enum OutFormat {
+    #[default]
+    Xlsx,
+    Csv,
+}
+// uses an idea from https://jwodder.github.io/kbits/posts/clap-bool-negate/
+
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about, long_about = None, styles=STYLES)]
+struct Cli {
+    in_file: PathBuf,
+
+    #[arg(long, short)]
+    out_file: Option<PathBuf>,
+
+    #[arg(long = "no-coerce", action=ArgAction::SetFalse)]
+    coerce: bool,
+
+    #[arg(long = "coerce", overrides_with = "coerce")]
+    _no_coerce: bool,
+
+    #[arg(long, short)]
+    format: Option<OutFormat>,
+}
+
 fn main() -> Result<(), Error> {
+    let cli = Cli::parse();
+
     // let convert_category = false;
-    let pq_file = File::open("data/example_parquet.parquet")?;
+    let pq_file = File::open(&cli.in_file)?;
     let mut df = ParquetReader::new(pq_file).set_rechunk(true).finish()?;
 
     let mut casts = vec![];
-    for (col_num, column) in df.get_columns().iter().enumerate() {
-        match map_supported(column.dtype()) {
-            Conversion::Pass => {}
-            Conversion::Convert(target) => {
-                casts.push(nth(col_num.try_into()?).cast(target));
+    if cli.coerce {
+        for (col_num, column) in df.get_columns().iter().enumerate() {
+            match map_supported(column.dtype()) {
+                Conversion::Pass => {}
+                Conversion::Convert(target) => {
+                    casts.push(nth(col_num.try_into()?).cast(target));
+                }
+                Conversion::Error(tt) => bail!("Unsupported data type: {tt:?}"),
+                Conversion::Process(dtype) => {
+                    casts.push(process(nth(col_num.try_into()?), &dtype)?);
+                }
             }
-            Conversion::Error(tt) => bail!("Unsupported data type: {tt:?}"),
-            Conversion::Process(dtype) => casts.push(process(nth(col_num.try_into()?), &dtype)?),
         }
     }
-    dbg!(&casts);
     if !casts.is_empty() {
         df = df.lazy().with_columns(casts).collect()?;
     }
-    // Create a new Excel writer.
-    let mut xlsx_writer = PolarsXlsxWriter::new();
+    match cli
+        .format
+        .unwrap_or_else(|| format_from_file(cli.out_file.as_deref()).unwrap_or_default())
+    {
+        OutFormat::Csv => todo!(),
+        OutFormat::Xlsx => {
+            // Create a new Excel writer.
+            let mut xlsx_writer = PolarsXlsxWriter::new();
 
-    // Write the dataframe to Excel.
-    xlsx_writer.write_dataframe(&df)?;
+            // Write the dataframe to Excel.
+            xlsx_writer.write_dataframe(&df)?;
 
-    // Save the file to disk.
-    xlsx_writer.save("data/dataframe.xlsx")?;
-    // let pq_file = File::create("dataframe.parquet")?;
-    // ParquetWriter::new(pq_file).finish(&mut df)?;
+            // Save the file to disk.
+            let out_file = cli
+                .out_file
+                .clone()
+                .unwrap_or_else(|| cli.in_file.with_extension("xlsx"));
+            xlsx_writer.save(out_file)?;
+        }
+    }
 
     Ok(())
+}
+
+fn format_from_file(p: Option<&Path>) -> Option<OutFormat> {
+    let ext = p.and_then(Path::extension);
+    match ext.and_then(|e| e.to_str()) {
+        Some("xlsx") => Some(OutFormat::Xlsx),
+        Some("csv") => Some(OutFormat::Csv),
+        _ => None,
+    }
 }
